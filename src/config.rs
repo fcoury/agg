0d6571d4fc1ecfd05438db0e7183436478f07c25
Felipe Coury <felipe.coury@gmail.com>
2025-01-08 10:50:05 -0300

    feat: uses .aggconfig if one is preset

---
+++ src/config.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AggConfig {
+    #[serde(default)]
+    pub include_binary: bool,
+    pub path: Option<PathBuf>,
+    pub output: Option<PathBuf>,
+    #[serde(default)]
+    pub exclude_dirs: Vec<String>,
+    #[serde(default)]
+    pub allowed_extensions: Vec<String>,
+}
+
+impl AggConfig {
+    pub fn load() -> Option<Self> {
+        let config_path = PathBuf::from(".aggconfig");
+        if config_path.exists() {
+            match fs::read_to_string(config_path) {
+                Ok(contents) => match toml::from_str(&contents) {
+                    Ok(config) => Some(config),
+                    Err(e) => {
+                        eprintln!("Error parsing .aggconfig: {}", e);
+                        None
+                    }
+                },
+                Err(e) => {
+                    eprintln!("Error reading .aggconfig: {}", e);
+                    None
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
